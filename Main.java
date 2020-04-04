059d4f7489ba53d0539429889dbfea2df55382ea
william <wahlbergwille@gmail.com>
2020-04-04 13:30:35 +0200

    add to countryStat

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
   public static void main(string[] blabla){
-    
+    lefiih = readFile();
   }
+
+  public static countryStat[] readFile(){
+    scanner
+    return 
+  }
+
 }
--- countryStat.java
+++ countryStat.java
@@ -1,6 +1,12 @@
 public class countryStat{
   String countryName
+  int[] dates;
+  int[] deaths;
+  int[] recoverd;
   public countryStat(String countryN){
     countryName = countryN;
+  }
+  public void addinfo(){
+
   }
 }
