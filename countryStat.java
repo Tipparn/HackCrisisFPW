072ce1f844c108d8d75fa557e40b53cb4b5422bc
Tipparn <43550846+Tipparn@users.noreply.github.com>
2020-04-04 22:27:43 +0200

    added total

--- countryStat.java
+++ countryStat.java
@@ -1,17 +1,21 @@
 import java.util.ArrayList;
 public class countryStat{
  	public String countryName;
-	public ArrayList<Date> date=new ArrayList<Date>();
+	public ArrayList<Date> date=new ArrayList<Date>();//skulle nog va vanliga arrayer??
 	public ArrayList<Integer> new_deaths=new ArrayList<Integer>();
 	public ArrayList<Integer> new_cases=new ArrayList<Integer>();
 	public ArrayList<Integer> sick=new ArrayList<Integer>();
+	public int total_death;// will add everything
+	public int total_cases;
 
   /*
-      en countryStat innehåller data för ett land
+      en countryStat innehåller data för ett land plus dagar
   */
 
   	public countryStat(String countryN){
     	countryName = countryN;
+	total_death = 0;// start the value from 0
+	total_cases = 0;
   	}
   	public void addinfo(String day, String month,String year,String new_case, String deaths ){
   		date.add(new Date(Integer.parseInt(year),Integer.parseInt(month),Integer.parseInt(day)));
