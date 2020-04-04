a76a6d9406ee082d1415dcaac1f33cc1313e17ea
fredrik kortetjärvi <fredrikkortetjarvi@protonmail.com>
2020-04-04 14:04:36 +0200

    fixed main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;  // Import the Scanner class
 import java.util.ArrayList;
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class Main{
 
 
-  public static void main(string[] blabla){
-    lefiih = readFile();
-
+  public static void main(string[] args){
+	Inputsteam in = new URL("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv").openStream();
+	Files.copy(in, Paths.get(data.txt), StandardCopyOption.REPLACE_EXISTING);
 
 
   }
 
-  public static ArrayList<> readFile(File file){
+  public static ArrayList<CountryStat> readFile(File file){
     Scanner sc = new Scanner(file);
     ArrayList<countryStat> all = new ArrayList<countryStat>();
 
-    while(sc.hasNext){
+    while(sc.hasNext()){
 
     }
-    return
+    return null;
   }
 
 }
