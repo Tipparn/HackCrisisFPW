b8b5649f5aff46c62f01f8adf1cb794e87e6b1f8
Philip Lundström <philip.lundstrom@live.se>
2020-04-04 14:24:18 +0200

    added lots of stuff

--- countryStat.java
+++ countryStat.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Date;
 public class countryStat{
-<<<<<<< HEAD
  	String countryName;
 	ArrayList<Date> date=new ArrayList<Date>();
 	ArrayList<Integer> deaths=new ArrayList<Integer>();
@@ -12,16 +11,5 @@ public class countryStat{
   	}
   	public void addinfo(String day, String month,String year,String new_case, String deaths ){
   		date.add(
-=======
-  String countryName
-  ArrayList<int> dates;
-  int[] deaths;
-  int[] recoverd;
-  public countryStat(String countryN){
-    countryName = countryN;
-  }
-  public void addinfo(){
->>>>>>> a76a6d9406ee082d1415dcaac1f33cc1313e17ea
-
   	}
 }
