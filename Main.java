8f37d6b802153ce1638a8aee5181e8d84cb9de0c
william <wahlbergwille@gmail.com>
2020-04-04 13:31:58 +0200

    add to countryStat

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 public class Main{
   public static void main(string[] blabla){
     lefiih = readFile();
+
+
+
   }
 
   public static countryStat[] readFile(){
-    scanner
-    return 
+    scanner sc = new scanner();
+    return
   }
 
 }
