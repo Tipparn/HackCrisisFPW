da70b517f7599da0d07acd00f051d1e511d2053d
william <wahlbergwille@gmail.com>
2020-04-04 16:13:53 +0200

    k

--- Main.java
+++ Main.java
@@ -10,13 +10,16 @@ public class Main{
 
 
   public static void main(String[] args){
-	//Inputsteam in = new URL("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv").openStream();
-	//Files.copy(in, Paths.get(data.txt), StandardCopyOption.REPLACE_EXISTING);
+	Inputsteam in = new URL("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv").openStream();
+	Files.copy(in, Paths.get(data.txt), StandardCopyOption.REPLACE_EXISTING);
 
   ArrayList<countryStat> data = readFile();
 
   }
 
+
+  /* readFile läser allt som finns i data.txt och
+     returnerar en arraylist<countryStat>    */
   public static ArrayList<countryStat> readFile(){
     ArrayList<countryStat> all = new ArrayList<countryStat>();
     try{
@@ -25,9 +28,9 @@ public class Main{
 
     String Name = "";
     countryStat cs = new countryStat("blabla");
-
     while(sc.hasNext()){
-      if(Name != cs.countryName){
+
+      if(Name.equals(cs.countryName)){
         all.add(cs);
         cs = new countryStat("blabla");
       }
@@ -37,16 +40,17 @@ public class Main{
       for(int i = 11; i<s.length();i++){
         t = t + s.charAt(i);
       }
+
       String[] array = t.split(",");
+
       Name= array[5];
-      
 
-
-
+      cs.addinfo(array[0],array[1],array[2],array[3],array[4]); // <-- något konstigt
     }
     }catch (Exception e) {
     System.out.println("fel");
   }
+    System.out.println(all);
     return all;
   }
 
--- countryStat.java
+++ countryStat.java
@@ -5,6 +5,11 @@ public class countryStat{
 	public ArrayList<Integer> new_deaths=new ArrayList<Integer>();
 	public ArrayList<Integer> new_cases=new ArrayList<Integer>();
 	public ArrayList<Integer> sick=new ArrayList<Integer>();
+
+  /*
+      en countryStat innehåller data för ett land
+  */
+
   	public countryStat(String countryN){
     	countryName = countryN;
   	}
@@ -16,8 +21,8 @@ public class countryStat{
 
     public String toString(){
       String s = this.countryName + "\n";
-      for(int i = 0;i< date.length();i++){
-        s = s + date.get(i); + "\t" + new_cases.get(i) + "\t" + new_deaths.get(i) + "\n";
+      for(int i = 0; i< date.size(); i++){
+        s = s + date.get(i) + "\t" + new_cases.get(i) + "\t" + new_deaths.get(i) + "\n";
       }
       return s;
     }
