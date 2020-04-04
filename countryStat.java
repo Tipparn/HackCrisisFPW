d95291173980cebdd07664a927f310f0e19915fa
Philip Lundström <philip.lundstrom@live.se>
2020-04-04 14:45:30 +0200

    addinfo

--- countryStat.java
+++ countryStat.java
@@ -3,13 +3,15 @@ import java.util.Date;
 public class countryStat{
  	String countryName;
 	ArrayList<Date> date=new ArrayList<Date>();
-	ArrayList<Integer> deaths=new ArrayList<Integer>();
-	ArrayList<Integer> new_case=new ArrayList<Integer>();
+	ArrayList<Integer> new_deaths=new ArrayList<Integer>();
+	ArrayList<Integer> new_cases=new ArrayList<Integer>();
 	ArrayList<Integer> sick=new ArrayList<Integer>();
   	public countryStat(String countryN){
     	countryName = countryN;
   	}
   	public void addinfo(String day, String month,String year,String new_case, String deaths ){
-  		date.add(
+  		date.add(new Date(Integer.parseInt(year),Integer.parseInt(month),Integer.parseInt(day)));
+  		new_cases.add(Integer.parseInt(new_case));
+  		new_deaths.add(Integer.parseInt(deaths));
   	}
 }
